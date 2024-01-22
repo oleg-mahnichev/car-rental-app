79f7aff7312af6ce990458f6188a5ea1b66e73e5
Oleg Makhnichev <oleg.mahnichev@gmail.com>
2024-01-22 21:01:05 +0100

    not_end

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import HomePage from './pages/Home/HomePage';
--- src/redux/reducers/favoritesReducer.js
+++ src/redux/reducers/favoritesReducer.js
@@ -1,26 +1,26 @@
 import { ActionTypes } from '../constants/actionTypes';
 
 const initialState = {
-    favorites: [], // Инициализация пустого массива
+    favorites: [],
 };
 
 const favoritesReducer = (state = initialState, action) => {
-
-
     switch (action.type) {
-        case ActionTypes.ADD_TO_FAVORITES:
+        case ActionTypes.ADD_TO_FAVORITES: {
             const newFavorite = action.payload;
             return {
                 ...state,
                 favorites: [...state.favorites, newFavorite],
             };
+        }
 
-        case ActionTypes.REMOVE_FROM_FAVORITES:
+        case ActionTypes.REMOVE_FROM_FAVORITES: {
             const removedFavoriteId = action.payload.id;
             return {
                 ...state,
                 favorites: state.favorites.filter((item) => item.id !== removedFavoriteId),
             };
+        }
 
         default:
             return state;
@@ -28,4 +28,3 @@ const favoritesReducer = (state = initialState, action) => {
 };
 
 export default favoritesReducer;
-
