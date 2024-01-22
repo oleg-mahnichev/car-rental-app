faa085fefd97488b90554513772e73b3dedd7a44
Oleg Makhnichev <oleg.mahnichev@gmail.com>
2024-01-22 21:22:39 +0100

    not_finish_hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './redux/store';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/car-rental-app">
+      <HashRouter>
+        {/* <BrowserRouter basename="/car-rental-app"> */}
         <Provider store={store}>
           <App />
         </Provider>
-      </BrowserRouter>
+        {/* </BrowserRouter> */}
+      </HashRouter>
     </PersistGate>
   </React.StrictMode>
 );
